ffe596445ef232bb9ddb047c2375cbc1e247773a
Asia Kane and Kai de Rochemont <kai@>
2017-05-24 10:58:09 -0700

    create initial file structure

---
+++ tmp/allConcat.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('#generate-dino').click(function() {
+    var paragraphs = parseInt($('#paragraphs').val());
+    var words = parseInt($('#words').val());
+    console.log(paragraphs);
+  })
+});
